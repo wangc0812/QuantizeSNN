60909a990b10a563609d909492fc568db4347b02
wangc0812 <leoncongwang@gmail.com>
2024-08-21 22:47:19 -0400

    first commit

---
+++ QLayer/QConv2d.py
@@ -0,0 +1,224 @@
+# read configuration file as global settings
+import configparser
+import os
+
+QuantizationMode  =   'LSQ'
+
+
+import torch
+import torch.nn as nn
+from torch.autograd.function import Function
+import torch.nn.functional as F
+import numpy as np
+
+if QuantizationMode == "WAGE" :
+    from Quantizer.WAGEQuantizer import WAGEQuantizer as Quantizer
+if QuantizationMode == "WAGEV2" :
+    from Quantizer.WAGEV2Quantizer import WAGEV2Quantizer as Quantizer    
+if QuantizationMode == "DynamicFixedPoint":
+    from Quantizer.DFQuantizer import DFQuantizer as Quantizer
+if QuantizationMode == "DynamicAffine":
+    from Quantizer.DAQuantizer import DAQuantizer as Quantizer
+if QuantizationMode == "LSQ":
+    from Quantizer.LSQuantizer import LSQuantizer as Quantizer
+if QuantizationMode == "LSQv2":
+    from Quantizer.LSQv2Quantizer import LSQPlusuantizer as Quantizer       
+
+
+
+
+class QConv2d(nn.Conv2d):
+    """docstring for QConv2d."""
+
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 stride=1, padding=0, dilation=1, groups=1,bias=False,
+                 quantize_weight=True, quantize_input= True, quantize_error=True,
+                 name ='Qconv2d'):
+
+        super(QConv2d, self).__init__(in_channels, out_channels, kernel_size,
+                                      stride, padding, dilation, groups, bias)
+        self.name = name
+        self.quantizer = Quantizer() 
+        self.scale = self.quantizer.weight_init(self.weight, factor=1.0)
+        self.quantize_weight = quantize_weight
+        self.quantize_input = quantize_input
+        self.quantize_error = quantize_error
+
+    def forward(self, input):
+        
+        
+        if self.quantize_input:
+            if self.training:
+                self.quantizer.update_range(input) 
+            input = self.quantizer.input_clamp(input)
+        output= CIM_Conv.apply(input, self.weight, self.bias, self.stride[0], self.padding[0], self.dilation, self.groups, self.scale,
+                              self.quantize_weight,self.quantize_input,self.quantize_error, self.name, self.quantizer, self.training)
+        return output
+
+    def extra_repr(self):
+        # (Optional)Set the extra information about this module. You can test
+        # it by printing an object of this class.
+        return 'kernel_size={}, in_channels={}, out_channels={}, stride={}, bias={}, quantize_weight={}, quantize_input={}, quantize_error={}'.format(
+            self.kernel_size,self.in_channels, self.out_channels,self.stride,self.bias is not None,self.quantize_weight , self.quantize_input, self.quantize_error
+        )
+
+
+class CIM_Conv(Function):
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    # bias is an optional argument
+    def forward(ctx, input, weight, bias=None, stride = 1, padding = 1,dilation=1, groups=1, fixed_scale=1,
+                quantize_weight =True,quantize_input =True,quantize_error =True, name = 'CONV',quantizer=None,training=None):
+        ctx.save_for_backward(input, weight, bias)
+        ctx.padding = padding
+        ctx.stride = stride
+        ctx.quantizer = quantizer
+        ctx.quantize_weight = quantize_weight
+        ctx.quantize_input = quantize_input
+        ctx.quantize_error = quantize_error
+        ctx.fixed_scale = fixed_scale
+        ctx.name = name
+        ctx.training = training
+        inputscale  = 1
+        weightscale = 1
+        inputshift  = 0
+        weightshift = 0
+        
+        if quantize_weight:
+            #mapping quantized weight to the code book
+            weight, weightscale, weightrange, weightshift = quantizer.QuantizeWeight(weight=weight, Wsigned=True,train=training)
+
+        if quantize_input:
+            #mapping quantized weight to the code book
+            input,  inputscale, inputrange, inputshift =  quantizer.QuantizeInput(input=input, Isigned=True,train=training)
+
+       
+        output = INTConv(input, inputshift, weight, weightshift, bias, stride, padding, dilation, groups)
+
+        return output*weightscale*inputscale/fixed_scale
+    
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, weight, bias= ctx.saved_tensors
+        padding = ctx.padding
+        quantizer = ctx.quantizer
+        inputscale = 1
+        weightscale = 1
+        grad_outputscale = 1
+        grad_bias = grad_input = grad_weight = None
+        pad_m = nn.ConstantPad2d(padding, 0)
+        grad_input = torch.zeros_like(input)
+        grad_input_dummy = torch.zeros_like(input)
+        dummy_grad_output = torch.zeros_like(grad_output)
+        dummy_weight = torch.zeros_like(weight)
+        dummy_input = torch.zeros_like(input)
+        grad_weight_dummy1 = torch.zeros_like(weight)
+        grad_weight_dummy2 = torch.zeros_like(weight)
+        grad_weight_dummy3 = torch.zeros_like(weight)
+        inputshift = 0
+        grad_outputshift = 0
+        weightshift = 0
+
+        if ctx.stride != 1:
+            grad_output = pad_within(grad_output,ctx.stride)
+
+        dummy_grad_output = torch.zeros_like(grad_output)
+        if ctx.quantize_weight:
+            #TODO: Pay attention to the range
+            weight, weightscale, weightrange, weightshift = quantizer.QuantizeWeight(weight=weight,Wsigned=True,train=ctx.training)
+
+            dummy_weight = torch.ones_like(weight)
+        if ctx.quantize_input:
+            #TODO: Pay attention to the range
+            input,  inputscale, inputrange, inputshift = quantizer.QuantizeInput(input=input,Isigned=True,train=ctx.training) 
+
+            dummy_input = torch.ones_like(input)
+
+        if ctx.quantize_error:
+            #np.save(Logdir+'/'+ctx.name+'_error.npy', input.cpu().numpy().dataq
+            # grad_output = torch.clamp(grad_output/grad_output.abs().max(), -1, 1)
+            #TODO: Pay attention to the range
+            
+            grad_output, grad_outputscale, grad_outputrange, grad_outputshift = quantizer.QuantizeError(error=grad_output,Esigned=True)
+        
+            dummy_grad_output = torch.ones_like(grad_output)
+
+        weight_t = weight.transpose(0, 1)
+        dummy_weight_t = dummy_weight.transpose(0, 1)
+        filter_size = weight_t.size()
+
+        input_size = input.size()
+        grad_output_size = grad_output.size()
+
+        if ctx.needs_input_grad[0]:
+            for i in range(filter_size[2]):
+                for j in range(filter_size[3]):
+                    current_filter = weight_t[:,:,filter_size[2]-1-i,filter_size[3]-1-j]
+                    current_filter_dummy = dummy_weight_t[:,:,filter_size[2]-1-i,filter_size[3]-1-j]
+
+                    output = torch.mm(current_filter,grad_output.transpose(0,1).reshape(grad_output_size[1],-1)).reshape(input_size[1],input_size[0],input_size[2],input_size[3]).transpose(0,1)
+                    output_dummy1 = torch.mm(current_filter,
+                                      dummy_grad_output.transpose(0, 1).reshape(grad_output_size[1], -1)).reshape(
+                        input_size[1], input_size[0], input_size[2], input_size[3]).transpose(0, 1) * grad_outputshift
+                    output_dummy2 = torch.mm(current_filter_dummy,
+                                      grad_output.transpose(0, 1).reshape(grad_output_size[1], -1)).reshape(
+                        input_size[1], input_size[0], input_size[2], input_size[3]).transpose(0, 1) * weightshift
+                    output_dummy3 = torch.mm(current_filter_dummy,
+                                             dummy_grad_output.transpose(0, 1).reshape(grad_output_size[1], -1)).reshape(
+                        input_size[1], input_size[0], input_size[2], input_size[3]).transpose(0, 1) * weightshift * grad_outputshift
+
+
+                    partial_output_dec = pad_m(output)
+                    partial_output_dec_dummy1 = pad_m(output_dummy1)
+                    partial_output_dec_dummy2 = pad_m(output_dummy2)
+                    partial_output_dec_dummy3 = pad_m(output_dummy3)
+
+                    grad_input = grad_input + partial_output_dec[:, :, i:i + input_size[2], j:j + input_size[3]]
+                    grad_input_dummy = grad_input_dummy + partial_output_dec_dummy1[:, :, i:i + input_size[2], j:j + input_size[3]]
+                    grad_input_dummy = grad_input_dummy + partial_output_dec_dummy2[:, :, i:i + input_size[2], j:j + input_size[3]]
+                    grad_input_dummy = grad_input_dummy + partial_output_dec_dummy3[:, :, i:i + input_size[2], j:j + input_size[3]]
+
+        if ctx.needs_input_grad[1]:
+            grad_weight = torch.zeros_like(weight)
+            padded_input = pad_m(input)
+            padded_input_dummy = pad_m(dummy_input)
+            grad_output_w = (grad_output).transpose(0,1).reshape(grad_output_size[1], -1)
+            grad_output_w_dummy = (dummy_grad_output).transpose(0,1).reshape(grad_output_size[1], -1)
+
+            for i in range(filter_size[2]):
+                for j in range(filter_size[3]):
+                    input_window = padded_input[:, :, i:i + input_size[2], j:j + input_size[3]].transpose(0,1).reshape(input_size[1], -1).transpose(0, 1)
+                    grad_weight[:, :, i, j] = torch.mm(grad_output_w,input_window)
+                    #input_window = padded_input[:, :, i:i + input_size[2], j:j + input_size[3]].transpose(0,1).reshape(input_size[1], -1).transpose(0, 1)
+                    grad_weight_dummy1[:, :, i, j] = torch.mm(grad_output_w_dummy,input_window)*grad_outputshift
+
+                    input_window = padded_input_dummy[:, :, i:i + input_size[2], j:j + input_size[3]].transpose(0,1).reshape(input_size[1], -1).transpose(0, 1)
+                    grad_weight_dummy2[:, :, i, j] = torch.mm(grad_output_w,input_window)*inputshift
+                    #input_window = padded_input[:, :, i:i + input_size[2], j:j + input_size[3]].transpose(0,1).reshape(input_size[1], -1).transpose(0, 1)
+                    grad_weight_dummy3[:, :, i, j] = torch.mm(grad_output_w_dummy,input_window)*grad_outputshift*inputshift
+        # return (grad_input+grad_input_dummy)*weightscale*grad_outputscale , (grad_weight+grad_weight_dummy1+grad_weight_dummy2+grad_weight_dummy3)*inputscale*grad_outputscale, grad_bias, None, None, None, None, None, None, None, None, None, None, None
+        # print("wg=",(grad_weight+grad_weight_dummy1+grad_weight_dummy2+grad_weight_dummy3)*inputscale*grad_outputscale)
+        return (grad_input+grad_input_dummy)*weightscale*grad_outputscale/ctx.fixed_scale , (grad_weight+grad_weight_dummy1+grad_weight_dummy2+grad_weight_dummy3)*inputscale*grad_outputscale, grad_bias, None, None, None, None, None, None, None, None, None, None, None
+
+
+def pad_within(x, stride=2):
+    w = x.new_zeros(stride, stride)
+    w[0, 0] = 1
+    return F.conv_transpose2d(x, w.expand(x.size(1), 1, stride, stride), stride=stride, groups=x.size(1))
+
+def INTConv(input, inputshift, weight, weightshift, bias, stride, padding, dilation, groups):
+     output = F.conv2d(input, weight, bias, stride, padding, dilation, groups).detach()
+     if inputshift != 0:
+          dummy_input = torch.ones_like(input)
+          output += inputshift * F.conv2d(dummy_input, weight, bias, stride, padding, dilation, groups).detach()
+     if weightshift != 0:
+          dummy_weight = torch.ones_like(weight)
+          output += weightshift * F.conv2d(input, dummy_weight, bias, stride, padding, dilation, groups).detach()
+     if inputshift != 0 and weightshift != 0 :
+          dummy_input = torch.ones_like(input)
+          dummy_weight = torch.ones_like(weight)
+          output += inputshift * weightshift * F.conv2d(dummy_input, dummy_weight, bias, stride, padding, dilation,
+                                                        groups).detach()
+     return output
+ 
+ 
---
+++ Quantizer/LSQv2_old.py
@@ -0,0 +1,255 @@
+from .Quantizer import Quantizer
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from torch.autograd import Function
+
+class Round(Function):
+    @staticmethod
+    def forward(self, input):
+        sign = torch.sign(input)
+        output = sign * torch.floor(torch.abs(input) + 0.5)
+        return output
+
+    @staticmethod
+    def backward(self, grad_output):
+        grad_input = grad_output.clone()
+        return grad_input
+
+class WLSQPlus(Function):
+    @staticmethod
+    def forward(ctx, weight, alpha, g, Qn, Qp, per_channel=False):
+        #根据论文里LEARNED STEP SIZE QUANTIZATION第2节的公式
+        # assert alpha > 0, "alpha={}".format(alpha)
+        ctx.save_for_backward(weight, alpha)
+        ctx.other = g, Qn, Qp, per_channel
+        if per_channel:
+            sizes = weight.size()
+            weight = weight.contiguous().view(weight.size()[0], -1)
+            weight = torch.transpose(weight, 0, 1)
+            alpha = torch.broadcast_to(alpha, weight.size())
+            w_q = Round.apply(torch.div(weight, alpha).clamp(Qn, Qp))
+            w_q = w_q * alpha
+            w_q = torch.transpose(w_q, 0, 1)
+            w_q = w_q.contiguous().view(sizes)
+        else:
+            w_q = Round.apply(torch.div(weight, alpha).clamp(Qn, Qp))
+            # w_q = w_q * alpha
+        return w_q, alpha
+
+    @staticmethod
+    def backward(ctx, grad_weight):
+        #根据论文里LEARNED STEP SIZE QUANTIZATION第2.1节
+        #分为三部分：位于量化区间的、小于下界的、大于上界的
+        weight, alpha = ctx.saved_tensors
+        g, Qn, Qp, per_channel = ctx.other
+        if per_channel:
+            sizes = weight.size()
+            weight = weight.contiguous().view(weight.size()[0], -1)
+            weight = torch.transpose(weight, 0, 1)
+            alpha = torch.broadcast_to(alpha, weight.size())
+            q_w = weight / alpha
+            q_w = torch.transpose(q_w, 0, 1)
+            q_w = q_w.contiguous().view(sizes)
+        else:
+            q_w = weight / alpha
+            
+        smaller = (q_w < Qn).float() #bool值转浮点值，1.0或者0.0
+        bigger = (q_w > Qp).float() #bool值转浮点值，1.0或者0.0
+        between = 1.0 - smaller -bigger #得到位于量化区间的index
+        if per_channel:
+            grad_alpha = ((smaller * Qn + bigger * Qp + 
+                between * Round.apply(q_w) - between * q_w)*grad_weight * g)
+            grad_alpha = grad_alpha.contiguous().view(grad_alpha.size()[0], -1).sum(dim=1)
+        else:
+            grad_alpha = ((smaller * Qn + bigger * Qp + 
+                between * Round.apply(q_w) - between * q_w)*grad_weight * g).sum().unsqueeze(dim=0)
+        #在量化区间之外的值都是常数，故导数也是0
+        grad_weight = between * grad_weight
+        return grad_weight, grad_alpha, None, None, None, None
+
+class ALSQPlus(Function):
+    @staticmethod
+    def forward(ctx, weight, alpha, g, Qn, Qp, beta):
+        # assert alpha > 0, "alpha={}".format(alpha)
+        ctx.save_for_backward(weight, alpha, beta)
+        ctx.other = g, Qn, Qp
+        w_q = Round.apply(torch.div((weight - beta), alpha).clamp(Qn, Qp))
+        # w_q = w_q * alpha + beta
+        return w_q,alpha,beta
+
+    @staticmethod
+    def backward(ctx, grad_weight,grad_alpha,grad_beta):
+        weight, alpha, beta = ctx.saved_tensors
+        g, Qn, Qp = ctx.other
+        q_w = (weight - beta) / alpha
+        smaller = (q_w < Qn).float() #bool值转浮点值，1.0或者0.0
+        bigger = (q_w > Qp).float() #bool值转浮点值，1.0或者0.0
+        between = 1.0 - smaller -bigger #得到位于量化区间的index
+        grad_alpha = ((smaller * Qn + bigger * Qp + 
+            between * Round.apply(q_w) - between * q_w)*grad_weight * g).sum().unsqueeze(dim=0)
+        grad_beta = ((smaller + bigger) * grad_weight * g).sum().unsqueeze(dim=0)
+        #在量化区间之外的值都是常数，故导数也是0
+        grad_weight = between * grad_weight
+        #返回的梯度要和forward的参数对应起来
+        return grad_weight, grad_alpha,  None, None, None, grad_beta
+
+class LSQPlusActivationQuantizer(nn.Module):
+    def __init__(self, a_bits, Isigned=False):
+        #activations 没有per-channel这个选项的
+        super(LSQPlusActivationQuantizer, self).__init__()
+        self.a_bits = a_bits
+        self.Isigned = Isigned
+        # self.batch_init = batch_init
+        if self.Isigned == False:
+            # unsigned activation is quantized to [0, 2^b-1]
+            self.Qn = 0
+            self.Qp = 2 ** self.a_bits - 1
+        else:
+            # signed weight/activation is quantized to [-2^(b-1), 2^(b-1)-1]
+            self.Qn = - 2 ** (self.a_bits - 1)
+            self.Qp = 2 ** (self.a_bits - 1) - 1
+        self.s = torch.nn.Parameter(torch.ones(1), requires_grad=True)
+        self.beta = torch.nn.Parameter(torch.tensor([float(-1e-9)]), requires_grad=True)
+        # self.beta = torch.nn.Parameter(torch.ones(0), requires_grad=True)
+        # self.s = torch.nn.Parameter(torch.ones(0.01), requires_grad=True)
+        # self.register_parameter('Ascale', self.s)
+        # self.init_state = 0
+
+    # 量化/反量化
+    def forward(self, activation):
+        '''
+        For this work, each layer of weights and each layer of activations has a distinct step size, represented
+as an fp32 value, initialized to 2h|v|i/√OP , computed on either the initial weights values or the first
+batch of activations, respectively
+        '''
+        #V1
+        # if self.init_state==0:
+        #     self.g = 1.0/math.sqrt(activation.numel() * self.Qp)
+        #     self.s.data = torch.mean(torch.abs(activation.detach()))*2/(math.sqrt(self.Qp))
+        #     self.init_state += 1
+        # elif self.init_state<self.batch_init:
+        #     self.s.data = 0.9*self.s.data + 0.1*torch.mean(torch.abs(activation.detach()))*2/(math.sqrt(self.Qp))
+        #     self.init_state += 1
+        # elif self.init_state==self.batch_init:
+        #     # self.s = torch.nn.Parameter(self.s)
+        #     self.init_state += 1
+        
+        self.g = 1.0/math.sqrt(activation.numel() * self.Qp)
+        # mina = torch.min(activation.detach())
+        # self.s.data = (torch.max(activation.detach()) - mina)/(self.Qp-self.Qn)
+        # self.beta.data = mina - self.s.data *self.Qn
+        
+        # self.s.data = torch.mean(torch.abs(activation.detach()))*2/(math.sqrt(self.Qp))
+        
+        # if self.a_bits == 32:
+        #     output = activation
+        # elif self.a_bits == 1:
+        #     print('！Binary quantization is not supported ！')
+        #     assert self.a_bits != 1
+        # else:
+            # print(self.s, self.g)
+        q_a,s_a,beta_a = ALSQPlus.apply(activation, self.s, self.g, self.Qn, self.Qp, self.beta)
+
+            # alpha = grad_scale(self.s, g)
+            # q_a = Round.apply((activation/alpha).clamp(Qn, Qp)) * alpha
+        return q_a,s_a,beta_a
+    
+class LSQPlusWeightQuantizer(nn.Module):
+    def __init__(self, w_bits, Wsigned=False):
+        super(LSQPlusWeightQuantizer, self).__init__()
+        self.w_bits = w_bits
+        self.Wsigned = Wsigned
+        # self.batch_init = batch_init
+        if self.Wsigned == False:
+            # unsigned activation is quantized to [0, 2^b-1]
+            self.Qn = 0
+            self.Qp = 2 ** w_bits - 1
+        else:
+            # signed weight/activation is quantized to [-2^(b-1), 2^(b-1)-1]
+            self.Qn = - 2 ** (w_bits - 1)
+            self.Qp = 2 ** (w_bits - 1) - 1
+        # self.per_channel = per_channel
+        self.s = torch.nn.Parameter(torch.ones(1), requires_grad=True)
+        # self.register_parameter('Wscale', self.s)
+        self.init_state = 0
+
+    # 量化/反量化
+    def forward(self, weight):
+        # if self.init_state==0:
+        #     self.g = 1.0/math.sqrt(weight.numel() * self.Qp)
+        #     if self.per_channel:
+        #         weight_tmp = weight.detach().contiguous().view(weight.size()[0], -1)
+        #         self.s.data = torch.mean(torch.abs(weight_tmp), dim=1)*2/(math.sqrt(self.Qp))
+        #     else:
+        #         self.s.data = torch.mean(torch.abs(weight.detach()))*2/(math.sqrt(self.Qp))
+        #     self.init_state += 1
+        # elif self.init_state<self.batch_init:
+        #     if self.per_channel:
+        #         weight_tmp = weight.detach().contiguous().view(weight.size()[0], -1)
+        #         self.s.data = 0.9*self.s.data + 0.1*torch.mean(torch.abs(weight_tmp), dim=1)*2/(math.sqrt(self.Qp))
+        #     else:
+        #         self.s.data = 0.9*self.s.data + 0.1*torch.mean(torch.abs(weight.detach()))*2/(math.sqrt(self.Qp))
+        #     self.init_state += 1
+        # elif self.init_state==self.batch_init:
+        #     # self.s = torch.nn.Parameter(self.s)
+        #     self.init_state += 1
+        
+        self.g = 1.0/math.sqrt(weight.numel() * self.Qp)
+        # self.s.data = torch.mean(torch.abs(weight.detach()))*2/(math.sqrt(self.Qp))
+        mean = torch.mean(weight.detach())
+        std = torch.std(weight.detach())
+        self.div = 2**self.w_bits - 1
+        self.s.data = max([torch.abs(mean-3*std), torch.abs(mean + 3*std)])/self.div
+        w_q,w_a = WLSQPlus.apply(weight, self.s, self.g, self.Qn, self.Qp)
+
+            # alpha = grad_scale(self.s, g)
+            # w_q = Round.apply((weight/alpha).clamp(Qn, Qp)) * alpha
+        return w_q,w_a
+
+
+class LSQPlusuantizer(Quantizer):
+    
+    def __init__(self):
+        super(LSQPlusuantizer, self).__init__()
+        self.activation_quantizer = LSQPlusActivationQuantizer(a_bits=self.input_precision, Isigned=self.Isigned)
+        self.weight_quantizer = LSQPlusWeightQuantizer(w_bits=self.weight_precision, Wsigned=self.Wsigned)
+
+    def weight_init(self, weight, bits_W=None,factor=2.0, mode="fan_in"):
+        scale = 1.0
+        return scale    
+    
+    def update_range(self, input):
+        pass
+    
+    def input_clamp(self, input):
+        return input  
+                      
+    def QuantizeWeight(self, weight, bits=None, Wsigned=True):
+        
+        weight, weightscale = self.weight_quantizer(weight)
+        weightrange = []
+        weightshift = 0.0
+        
+        return weight, weightscale, weightrange, weightshift 
+
+    def QuantizeInput(self, input, bits=None, Isigned=True):
+        
+        input,inputscale,inputshift = self.activation_quantizer(input) 
+        inputrange = []   
+        
+        return input, inputscale, inputrange, inputshift
+    
+    def QuantizeError(self, error, bits=None, Esigned=True):
+        if bits is  None:
+            bits= self.error_precision
+               
+        error, errorscale, errorrange,errorshift = self.Q(error,self.error_precision,signed=Esigned, 
+                                                          fixed_range=range,
+                                                          odd_stage=True)
+        
+        return error, errorscale, errorrange, errorshift 
+
+    def quantize_grad(self, x): 
+        raise NotImplementedError("use QSGD")
